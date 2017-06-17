c1d1ba126f1989136c09459322e8b4666f8874c5
Kenichi Takahashi <kenichi.taka@gmail.com>
2017-06-18 00:21:57 +0900

    style-loaderにfallbackしても意味が無いので削除する

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,6 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
           use: ['css-loader', 'sass-loader']
         })
       },
